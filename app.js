ee372d44de7ef1e11d9265816302ca1b03f7d7ac
Elvasfar <brondby_1964@hotmail.com>
2023-01-31 12:35:06 +0100

    Knap og tekstfelt tilføjet
    Man kan nu skrive sit navn og app'en vil nu skrive hej navn.

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-document.body.innerHTML += "Hello JavaScript!";
+document.body.innerHTML += "";
 //alert("Hello dat.js");
 console.log("Hello Dat.js");
 console.log("hej");
 console.log("min første app");
+
+function sayHello() {
+  document.querySelector("h3").textContent =
+    "Hej " + document.querySelector("input").value;
+}
+document.querySelector("button").addEventListener("click", sayHello);
